c52aba376f66ac3a2d4e8d33afca98895a28a0cc
Ayesha Shabbir <aishashabbirdatasci207@gmail.com>
2025-06-15 11:50:20 +0500

    DRF

---
+++ gs7/api/views.py
@@ -0,0 +1,83 @@
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import io
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+from .models import Student
+from .serializers import StudentSerializer
+from django.shortcuts import get_object_or_404
+from django.utils.decorators import method_decorator
+from django.views import View
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class StudentAPI(View):
+    def get(self, request, *args, **kwargs):
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+
+        id = pythondata.get('id', None)
+        if id is not None:
+            stu = Student.objects.get(id=id)
+            serializer = StudentSerializer(stu)  # Serialize single student
+            json_data = JSONRenderer().render(serializer.data)
+            return HttpResponse(json_data, content_type='application/json')
+
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many=True)  # Serialize multiple students
+        json_data = JSONRenderer().render(serializer.data)
+        return HttpResponse(json_data, content_type='application/json')
+
+    def post(self, request, *args, **kwargs):
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        serializer = StudentSerializer(data=pythondata)
+
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg': 'Data Created'}
+            json_data = JSONRenderer().render(res)
+            return HttpResponse(json_data, content_type='application/json')
+        json_data = JSONRenderer().render(serializer.errors)
+        return HttpResponse(json_data, content_type='application/json', status=400)
+
+    def put(self, request, *args, **kwargs):
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        id = pythondata.get('id')
+
+        stu = Student.objects.get(id=id)
+        serializer = StudentSerializer(stu, data=pythondata, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg': 'Data Updated !!'}
+            json_data = JSONRenderer().render(res)
+            return HttpResponse(json_data, content_type='application/json')
+        json_data = JSONRenderer().render(serializer.errors)
+        return HttpResponse(json_data, content_type='application/json', status=400)
+
+    def delete(self, request, *args, **kwargs):
+        if request.method == 'DELETE':
+            json_data = request.body
+            stream = io.BytesIO(json_data)
+            pythondata = JSONParser().parse(stream)
+            id = pythondata.get('id')
+
+            stu = get_object_or_404(Student, id=id)
+            res = {}
+            if stu:
+                stu.delete()
+                res = {'msg': 'Data Deleted !!'}
+            else:
+                res = {'msg': 'Data not found !!'}
+            json_data = JSONRenderer().render(res)
+            return HttpResponse(json_data, content_type='application/json')
+
+        return HttpResponse(
+            JSONRenderer().render({'error': 'Method not allowed'}),
+            content_type='application/json',
+            status=405
+        )
---
+++ gs7/myapp.py
@@ -0,0 +1,57 @@
+import requests
+import json
+
+URL = "http://127.0.0.1:8000/studentapi/"  
+
+headers = {
+    'Content-Type': 'application/json'
+}
+
+def get_data(id=None):
+    data = {}
+    if id is not None:
+        data = {'id': id}
+    json_data = json.dumps(data)
+    response = requests.get(url=URL, headers=headers, data=json_data)
+    print("GET Status Code:", response.status_code)
+    print("Response:", response.json())
+
+def post_data():
+    data = {
+        'name': 'areeba',
+        'roll': '45',
+        'city': 'fsl'
+    }
+    json_data = json.dumps(data)
+    response = requests.post(url=URL, headers=headers, data=json_data)
+    print("POST Status Code:", response.status_code)
+    print("Response:", response.json())
+
+def update_data():
+    data = {
+        'id': 8,
+        'name': 'jk',
+        'roll':'56',
+        'city': 'amarica',
+    }
+    json_data = json.dumps(data)
+    response = requests.put(url=URL, headers=headers, data=json_data)  # Use PUT for updates 
+    print("PUT Status Code:", response.status_code)
+    print("Response:", response.json())
+
+def delete_data():
+    data = {'id': 9}
+    json_data = json.dumps(data)
+    response = requests.delete(url=URL, headers=headers, data=json_data)
+    print("DELETE Status Code:", response.status_code)
+
+    if response.status_code==404:
+        print('Data not found')
+    else:
+        print("Response:",response.json())
+
+# Example function calls
+#get_data(3)
+#post_data()
+update_data()
+#delete_data()
