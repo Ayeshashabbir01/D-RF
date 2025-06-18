14c21fd06789575dc07d664d35339c2fe351d7df
Ayesha Shabbir <aishashabbirdatasci207@gmail.com>
2025-06-18 12:53:05 +0500

    DRF

--- gs21/api/views.py
+++ gs21/api/views.py
@@ -2,7 +2,7 @@ from .models import Student
 from rest_framework import viewsets
 from .serializers import StudentSerializer
 from rest_framework.authentication import SessionAuthentication
-from rest_framework.permissions import IsAuthenticated,AllowAny,IsAdminUser,IsAuthenticatedOrReadOnly, DjangoModelPermissions,DjangoModelPermissionsOrAnonReadOnly
+from .custompermissions import MyPermissions
 
 
 class StudentModelViewSet(viewsets.ModelViewSet):
@@ -12,9 +12,5 @@ class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     authentication_classes = [SessionAuthentication]
-    permission_classes = [IsAuthenticated]
-    #permission_classes = [AllowAny]  # Change to AllowAny to allow unauthenticated access
-    #permission_classes = [IsAdminUser]  # Change to IsAdminUser to restrict access to admin users only
-    #permission_classes = [IsAuthenticatedOrReadOnly]  # Allows read-only access to unauthenticated users
-    #permission_classes = [DjangoModelPermissions]  # Use Django's model permissions for access control
-    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]  # Allows read-only access to unauthenticated users, but requires permissions for write operations
+    permission_classes = [MyPermissions]
+   
